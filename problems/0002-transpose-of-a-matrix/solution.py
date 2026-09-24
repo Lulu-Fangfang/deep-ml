7d8711b49aa97d56e5ab8d277c5b3440d3be6348
Lulu-Fangfang <79155409+Lulu-Fangfang@users.noreply.github.com>
2026-09-24 06:53:48 +0000

    Solve: Transpose of a Matrix

---
+++ problems/0002-transpose-of-a-matrix/solution.py
@@ -0,0 +1,26 @@
+def transpose_matrix(a: list[list[int|float]]) -> list[list[int|float]]:
+    """
+    Transpose a 2D matrix by swapping rows and columns.
+    
+    Args:
+        a: A 2D matrix of shape (m, n)
+    
+    Returns:
+        The transposed matrix of shape (n, m)
+    """
+    # Your code here
+    if not a :
+        return []
+    rows = len(a)
+    cols = len(a[0])
+
+    result = []
+
+    for j in range(cols):
+        new_row = []
+        for i in range(rows):
+            new_row.append(a[i][j])
+        result.append(new_row)
+
+    return result
+    
